e46ac1c5473915444e4d1c0c9c6a2a8326e48fc9
Tulio Faria <tuliofaria@gmail.com>
2021-11-23 20:03:22 -0300

    add remix

---
+++ frontend-remix/server/index.js
@@ -0,0 +1,28 @@
+/*
+const { createRequestHandler } = require("@remix-run/vercel");
+
+module.exports = createRequestHandler({
+  build: require("./build")
+});
+*/
+const express = require('express')
+const { createRequestHandler } = require('@remix-run/express')
+
+let app = express()
+
+// needs to handle all verbs (GET, POST, etc.)
+app.all(
+  '*',
+  createRequestHandler({
+    // `remix build` and `remix dev` output files to a build directory, you need
+    // to pass that build to the request handler
+    build: require('./build'),
+
+    // return anything you want here to be available as `context` in your
+    // loaders and actions. This is where you can bridge the gap between Remix
+    // and your server
+    getLoadContext(req, res) {
+      return {}
+    }
+  })
+)
